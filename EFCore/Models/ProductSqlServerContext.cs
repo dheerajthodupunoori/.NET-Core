8e2515b77003860538570cecd132f1d1949164a9
dheeraj <dheeraj.thodupunoori01@gmail.com>
2019-11-08 19:04:04 +0530

    added appSettings.json file to solution and created new dbcontext and reading sql connection from config file(json file)

---
+++ EFCore/Models/ProductSqlServerContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFCore.Models
+{
+    public class ProductSqlServerContext : DbContext
+    {
+        private string connectionString;
+
+        public ProductSqlServerContext() : base()
+        {
+            var builder = new ConfigurationBuilder();
+            builder.AddJsonFile("appsettings.json", optional: false);
+            var configuration = builder.Build();
+            connectionString = configuration.GetConnectionString("SQLServerConnection").ToString();
+        }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
+
